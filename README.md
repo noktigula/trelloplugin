0ca23a838e54774b10cf98e00c459cbb333c6257
System <none>
2017-05-16 14:57:53 +0000

    Checkpoint

--- README.md
+++ README.md
@@ -1,27 +1,3 @@
-Welcome to the Glitch BETA
-=========================
-
-Click `Show` in the header to see your app live. Updates to your code will instantly deploy and update live.
-
-Glitch is a developer playground that lets you code a real web-app without the slow setup and deployment steps.
-
-[About Glitch](https://glitch.com/about)
 
 
-Your Project
-------------
-
-On the back-end,
-- your app starts at `server.js`
-- add frameworks and packages in `package.json`
-- safely store app secrets in `.env`
-
-On the front-end,
-- edit `public/client.js`, `public/style.css` and `views/index.html`
-- drag in `assets`, like images or music, to add them to your project
-
-
-Made by Fog Creek
------------------
-
-\ ゜o゜)ノ
+<div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
--- public/js/estimate.js
+++ public/js/estimate.js
@@ -13,43 +13,12 @@ document.getElementById('save').addEventListener('click', function(){
 });
 
 t.render(function(){
-  t.sizeTo('#content')
-  .done();
+  return t.get('card', 'shared', 'estimate')
+  .then(function(estimate){
+    estimateSelector.value = estimate;
+  })
+  .then(function(){
+    t.sizeTo('#content')
+    .done();
+  });
 });
-
-// var estimateSelector = document.getElementById('estimate');
-// var timeSelector = document.getElementById('time');
-// var versionSelector = document.getElementById('version');
-
-// t.render(function(){
-//   return Promise.all([
-//     t.get('card', 'shared', 'estimate', 0),
-//     t.get('card', 'shared', 'time', 0),
-//     t.get('card', 'shared', 'version', 'No version')
-//   ])
-//   .spread(function(estimate, time, version){
-//     estimateSelector.value = estimate;
-//     timeSelector.value = time;
-//     version.value = version;
-    
-//   })
-//   .then(function(){
-//     t.sizeTo('#content')
-//     .done();
-//   });
-// });
-
-// document.getElementById('save').addEventListener('click', function(){
-//   return t.set('card', 'shared', 'estimate', estimateSelector.value)
-//   .then(function(){
-//     return t.set('card', 'shared', 'time', timeSelector.value);
-//   }).then(function(){
-//     return t.set('card', 'shared', 'lastModified', new Date());
-//   })
-//   .then(function() {
-//     return t.set('card', 'shared', 'version', versionSelector.value);
-//   })
-//   .then(function(){
-//     t.closePopup();
-//   });
-// });
