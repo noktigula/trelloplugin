a02ecd1efea25bf56b996e69303d2eba44bb4f35
Glitch (trello-power-up-skeleton) <none>
2018-08-06 20:14:58 +0000

    🎑🚂 Checkpoint ./public/js/client.js:202855/5

--- public/js/client.js
+++ public/js/client.js
@@ -8,7 +8,7 @@ TrelloPowerUp.initialize({
   // Start adding handlers for your capabilities here!
 	// 'card-buttons': function(t, options) {
 	// 	return [{
-	// 		icon: BLACK_ROCKET_ICON,
+    // 		icon: BLACK_ROCKET_ICON,
 	// 		text: 'Estimate Size',
 	// callback: function(t) {
 	// return t.popup({
