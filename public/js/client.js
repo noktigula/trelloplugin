84819226a986f1135b42c7f66a6983f13023a5ed
Glitch (trello-power-up-skeleton) <none>
2019-01-14 11:28:05 +0000

    🎤😅 Checkpoint ./package.json:2795416/23 ./server.js:2795416/2 ./views/index.html:2795416/111 ./public/js/client.js:2795416/106

--- public/js/client.js
+++ public/js/client.js
@@ -5,9 +5,12 @@ var Promise = TrelloPowerUp.Promise;
 var BLACK_TIMER_ICON = 'https://cdn.glitch.com/93f19877-502c-49d7-86ca-fa817403bca7%2Ficons8-timer-black.png?1547464335418';
 var WHITE_TIMER_ICON = 'https://cdn.glitch.com/93f19877-502c-49d7-86ca-fa817403bca7%2Ficons8-timer-white.png?1547464335805';
 
+console.log('client.js started');
+
 TrelloPowerUp.initialize({
   // Start adding handlers for your capabilities here!
 	'card-buttons': function(t, options){
+    console.log('card-buttons called!');
     return [{
       icon: BLACK_TIMER_ICON,
       text: 'Estimate Size',
@@ -18,6 +21,5 @@ TrelloPowerUp.initialize({
         });
       }
     }];
-	})
 	},
 });
