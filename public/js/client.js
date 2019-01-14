2391ca343af6a160d2c60534e3ee3ba5b8a49b77
Glitch (trello-power-up-skeleton) <none>
2019-01-14 13:22:19 +0000

    ⛹😺 Checkpoint ./public/js/client.js:2774443/485:2795416/984

--- public/js/client.js
+++ public/js/client.js
@@ -14,10 +14,21 @@ TrelloPowerUp.initialize({
 	'card-buttons': function(t, options){
     t.get('card', 'shared', 'estimate')
     .then(function(estimate) {
-      var text = estimate ? 'Estimated: ' + estimate : 'Estimate time';
+      console.log('estimation found' + estimate);
+      if (!estimate) {
+        return;
+      }
       
-    })
-    
+      console.log('searching for button');
+      var aTags = document.getElementsByTagName('a');
+      for (var i = 0; i < aTags.length; i++) {
+        console.log('Checking element ' + aTags[i]);
+        if (aTags[i].title && aTags[i].title === 'Estimate time') {
+          aTags[i].title = 'Estimated' + estimate;
+          break;
+        }
+      }
+    });
     return [{
         icon: BLACK_TIMER_ICON,
         text: 'Estimate time',
@@ -33,10 +44,23 @@ TrelloPowerUp.initialize({
       return t.get('card', 'shared', 'estimate')
       .then(function(estimate) {
         return [{
-          icon: 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Frocket-ship-grey.png?1496162964717',
+          icon: 'https://cdn.glitch.com/93f19877-502c-49d7-86ca-fa817403bca7%2Fstorypoints-icon.png?1547471374757',
           color: estimate ? 'blue' : 'red',
           text: estimate ? estimate : 'No Estimation!' 
         }];
       })
-    }  
+  },
+  'card-detail-badges': function(t, options) {
+    return [{
+      title: 'Estimate',
+      color: 'red',
+      text: 'Large',
+      callback: function(t) {
+        return t.popup({
+          title: "Estimation",
+          url: '../estimate.html'
+        });
+      }      
+    }]
+  }  
 });
