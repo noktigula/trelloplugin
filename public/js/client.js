d684d2a5fde21c72a97fbb197cccda0447023129
Glitch (trello-power-up-skeleton) <none>
2019-01-14 17:19:51 +0000

    🚥⛪️ Checkpoint ./public/js/client.js:2774443/447

--- public/js/client.js
+++ public/js/client.js
@@ -48,23 +48,25 @@ TrelloPowerUp.initialize({
             console.log(cards.array);
             var total = 0;
             var promises = [];
-            promises.push(
+            
               cards.forEach(element => {
-                  t.get(element.id, 'shared', 'estimate')
-                  .then(function(estimate) {
-                      total += parseFloat(estimate);
-                    console.log(total); 
-                  });
-              })
-          );
+                  promises.push(
+                    t.get(element.id, 'shared', 'estimate')
+                    .then(function(estimate) {
+                        total += parseFloat(estimate);
+                      console.log(total); 
+                    })
+                  );
+              });
   
-          Promise.all(promises).then(() => 
+          Promise.all(promises).then(function() {
+            console.log('return is ' + total);
             return [{
                 icon: 'https://cdn.glitch.com/93f19877-502c-49d7-86ca-fa817403bca7%2Fstorypoints-icon.png?1547471374757',
                 text: 'Total Est: ' + total,
                 condition: 'always'
-            }];   
-          );
-        });
+            }]; 
+          });
+      });
   }  
 });
