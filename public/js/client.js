07343cf4d3ac3dbca7132214126305442bafe264
Glitch (trello-power-up-skeleton) <none>
2019-01-14 13:55:14 +0000

    👳💴 Checkpoint ./public/js/client.js:2774443/655

--- public/js/client.js
+++ public/js/client.js
@@ -27,30 +27,37 @@ TrelloPowerUp.initialize({
       }];
     })
 	},
+  /* Show on card */
   'card-badges': function(t, options) {
       return t.get('card', 'shared', 'estimate')
       .then(function(estimate) {
-        return [{
-          icon: 'https://cdn.glitch.com/93f19877-502c-49d7-86ca-fa817403bca7%2Fstorypoints-icon.png?1547471374757',
-          color: estimate ? 'blue' : 'red',
-          text: estimate ? estimate : 'No Estimation!' 
-        }];
+        return [
+          /* Estimation */
+          {
+            icon: 'https://cdn.glitch.com/93f19877-502c-49d7-86ca-fa817403bca7%2Fstorypoints-icon.png?1547471374757',
+            color: estimate ? 'blue' : 'red',
+            text: estimate ? estimate : 'No Estimation!' 
+          }
+        ];
       })
   },
   'card-detail-badges': function(t, options) {
     return t.get('card', 'shared', 'estimate')
     .then(function(estimate) {
-      return [{
-        title: 'Estimate',
-        color: estimate ? 'blue' : 'red',
-        text: estimate ? estimate : 'No Estimation!',
-        callback: function(t) {
-          return t.popup({
-            title: "Estimation",
-            url: '../estimate.html'
-          });
-        }      
-      }]
+      return [
+          /* Estimation */
+          {
+            title: 'Estimate',
+            color: estimate ? 'blue' : 'red',
+            text: estimate ? estimate : 'No Estimation!',
+            callback: function(t) {
+              return t.popup({
+                title: "Estimation",
+                url: '../estimate.html'
+              });
+            }      
+          }
+      ]
     })
   }  
 });
