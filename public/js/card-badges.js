019198da8583732e811a8f854b5148d60159caf4
Dot-sent <sergey.kudriavtsev@gmail.com>
2019-01-16 17:13:52 +0100

    Epics: added Epic badges and badges for the cards that are part of an Epic

--- public/js/card-badges.js
+++ public/js/card-badges.js
@@ -35,10 +35,42 @@ function progressBadge(trello, options) {
     });
 }
 
+function epicBadge(trello, options) {
+    return Promise.all([
+        trello.get('card', 'shared', 'epic'),
+        trello.get('board', 'shared', 'epics')
+    ])
+    .then(function(values) {
+        if (values[0] == '_self_') {
+            return {
+                title: 'EPIC',
+                icon: EPIC_ICON,
+                color: 'red',
+                text: 'EPIC'
+            }
+        } else if (values[0] != undefined && values[0] != '') {
+            var epics = values[1] == undefined || values[1] == '' ? [] : JSON.parse(values[1]);
+            if (Array.isArray(epics)) {
+                for (var i  = 0; i < epics.length; i++) {
+                    if (epics[i].id == values[0]) {
+                        return {
+                            title: 'EPIC',
+                            icon: EPIC_ICON,
+                            color: 'yellow',
+                            text: 'Part of: ' + epics[i].name
+                        }
+                    }
+                }
+            }
+        }
+    });
+}
+
 function cardBadges(trello, options) {
     return Promise.all([
         estimationBadge(trello),
-        progressBadge(trello)
+        progressBadge(trello),
+        epicBadge(trello)
     ]).then(function (values) {
         return values;
     })
