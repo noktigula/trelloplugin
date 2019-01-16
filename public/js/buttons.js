7bde1eed8895806a7fb1728d3d9c56d910a108d7
Dot-sent <sergey.kudriavtsev@gmail.com>
2019-01-16 14:41:30 +0100

    Epics: Display epic name on the button istead of ID

--- public/js/buttons.js
+++ public/js/buttons.js
@@ -33,18 +33,30 @@ function createProgressButton(trello) {
 function createEpicButton(trello) {
   return trello.get('card', 'shared', 'epic')
   .then(function(epic) {
-    return {
-      icon: FOLDER_ICON,
-      text: epic == '_self_' ? 'Is Epic' : epic == '' || epic == undefined ? 'No Epic assigned' : 'Epic: ' + epic,
-      callback: function(trello) {
-        return trello.popup({
-          title: "This task is...",
-          url: '../epics.html',
-          height: 185,
-          args: {epic: epic},
-        });
+    return trello.get('board', 'shared', 'epics')
+    .then(function(epicsText) {
+      var epicName = "";
+      var epics = epicsText == undefined || epicsText == '' ? [] : JSON.parse(epicsText);
+      if (Array.isArray(epics)) {
+        for (var i = 0; i < epics.length; i++) {
+          if (epics[i].id == epic) {
+            epic = epics[i].name;
+          }
+        }
       }
-    }
+      return {
+        icon: FOLDER_ICON,
+        text: epic == '_self_' ? 'Is Epic' : epic == '' || epic == undefined ? 'No Epic assigned' : 'Epic: ' + epic,
+        callback: function(trello) {
+          return trello.popup({
+            title: "This task is...",
+            url: '../epics.html',
+            height: 185,
+            args: {epic: epic},
+          });
+        }
+      }
+    });
   })
 }
 
